dfe06792683c2bda161ea8fae39938b17cec911c
doc HD <doc.divxm@gmail.com>
2018-03-21 09:59:51 +0300

    d855: Add maintainer and commonize deps
    Change-Id: I0921edba9f54d34ba7cda376fefa929780dfb4e6

--- aicp.mk
+++ aicp.mk
@@ -10,3 +10,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="g3_global_com" \
     BUILD_FINGERPRINT="lge/g3_global_com/g3:6.0/MRA58K/15351124649f4:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_global_com-user 6.0 MRA58K 15351124649f4 release-keys"
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="doc HD (semdoc)"
