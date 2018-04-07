2f3238b0901b664f8aedcdb3b337b4cbc975c3a0
firebird11 <hbgassel@gmail.com>
2018-04-07 09:21:29 +0200

    d855: set build fingerprint instead of override
    Change-Id: I175b3e54ea723c01ea282f2a45f14e0caec21d90

--- aicp.mk
+++ aicp.mk
@@ -8,8 +8,9 @@ PRODUCT_NAME := aicp_d855
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_global_com" \
-    BUILD_FINGERPRINT="lge/g3_global_com/g3:6.0/MRA58K/15351124649f4:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_global_com-user 6.0 MRA58K 15351124649f4 release-keys"
+
+BUILD_FINGERPRINT="lge/g3_global_com/g3:6.0/MRA58K/15351124649f4:user/release-keys"
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
