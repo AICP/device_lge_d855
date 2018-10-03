47cd538c69786d3079f827ec8103a44e91a1748b
doc HD <doc.divxm@gmail.com>
2018-10-03 10:47:42 +0300

    d855: AICPfy

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,4 +13,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_d855.mk
+    $(LOCAL_DIR)/aicp_d855.mk
